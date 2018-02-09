73717e690e6afe3f3a28a872e5f54b0330102374
Evan Wheeler <anon0mys87@gmail.com>
2018-02-08 21:00:26 -0700

    Palindrome lvl 3 and sentence reverser lvl 3 complete. Reverser needs refactor

--- string_games/string_games.rb
+++ string_games/string_games.rb
@@ -1,5 +1,28 @@
 require 'pry'
 
-class StringGame
+# Deals with different string manipulations
+class StringGames
+  def palindrome?(word)
+    (0...word.length).each do |index|
+      return false unless word[index] == word[-index - 1]
+    end
+  end
 
+  def sentence_reverser(sentence)
+    word = ''
+    new_sentence = ''
+    (sentence + ' ').each_char do |char|
+      if char == ' '
+        new_sentence.prepend(char + word)
+        word = ''
+      else
+        word << char
+      end
+    end
+    new_sentence[1..-1]
+  end
+
+  def encryption(string)
+    
+  end
 end
--- string_games/string_games_test.rb
+++ string_games/string_games_test.rb
@@ -3,6 +3,31 @@ require 'minitest/pride'
 require 'pry'
 require_relative 'string_games'
 
+# Tests string games class
 class StringGamesTest < Minitest::Test
+  def test_palindromes
+    game = StringGames.new
 
+    assert game.palindrome?('racecar')
+    refute game.palindrome?('anything')
+  end
+
+  def test_sentence_reversal
+    game = StringGames.new
+    given = 'This is a sentence'
+    expected = 'sentence a is This'
+
+    assert_equal expected, game.sentence_reverser(given)
+  end
+
+  def test_encryption
+    game = StringGames.new
+    given = 'aaaabbbbbcccccc'
+    expected = '4a5b6c'
+    given2 = 'abcccaaa'
+    expected2 = 'ab3c3a'
+
+    assert_equal expected, game.encryption(given)
+    assert_equal expected2, game.encryption(given2)
+  end
 end
